13e0b43dd4de20b6640e714ac23a213d44dd784d
psjfoster <85462085+psjfoster@users.noreply.github.com>
2022-04-07 22:49:38 +0100

    Attempt pull request with non-final theme-picker

--- fur-elise.js
+++ fur-elise.js
@@ -1,21 +1,3 @@
-/* theme selection */
-function toggleTheme() {
-  if (document.getElementById("theme").href.includes("-light")) {
-    let moon = document.getElementById("moon");
-    moon.style.cursor = "pointer";
-    moon.style.filter = "grayscale(100%)";
-    moon.onclick = turnDark;
-  }
-}
-
-function turnDark() {
-  let theme = document.getElementById("theme");
-  theme.href = theme.href.replace("-light", "-dark");
-  toggleTheme();
-  let moon = document.getElementById("moon");
-  moon.reset
-}
-
 /* on the job calculator */
 function calcMinimumHours() {
   const annualHolidayHours = document.getElementById("annual-holiday-hours").value;
@@ -33,7 +15,7 @@ function calcMinimumHours() {
 function adjustContractHours() {
   const annualHolidayHours = document.getElementById("annual-holiday-hours").value;
   const durationWeeks = document.getElementById("course-duration").value;
-
+  
   const calcHolidayHours = annualHolidayHours * (durationWeeks / 52);
   const minimumHoursMultiplier = 
       document.getElementById("minimum-otj-percent").value / 100;
@@ -42,7 +24,7 @@ function adjustContractHours() {
 
   // conditional checks in future?
   document.getElementById("contract-hours").value = 
-      (((plannedHours / minimumHoursMultiplier) + calcHolidayHours) / durationWeeks).toFixed(1);
+  (((plannedHours / minimumHoursMultiplier) + calcHolidayHours) / durationWeeks).toFixed(1);
   calcMinimumHours();
   animateSuccess();
 }
@@ -53,6 +35,40 @@ function animateSuccess() {
   
   /* void triggers "reflow" */
   void contract.offsetWidth
-
+  
   contract.classList.add("success"); 
+}
+
+/* theme selection */
+function checkTheme(dark) {
+  if (dark) {
+    goDark();
+  }
+}
+
+function chooseTheme(theme) {
+  if (theme.startsWith("-")) {
+    if (theme.endsWith("light")) {
+      goLight();
+    }
+    if (theme.endsWith("dark")) {
+      goDark();
+    }
+  } else {
+    let colour = theme.split("-")[0];
+      goColour(colour);
+  }
+}
+
+function goLight() {
+  let theme = document.getElementById("theme");
+  theme.href = theme.href.replace("-dark", "-light");
+}
+
+function goDark() {
+  let theme = document.getElementById("theme");
+  theme.href = theme.href.replace("-light", "-dark");
+}
+
+function goColour(colour) { 
 }
